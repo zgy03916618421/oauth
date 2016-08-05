450b1297a56ea60d10d26de8bd8efde332784e73
zgy03916618421 <576428698@qq.com>
2016-08-05 14:01:54 +0800

    qi xi

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/8/5.
+ */
+var config;
+config={
+    AppID:'wxdc041c85f0b7b6f',
+    AppSecret:'5bcb2ec82178f7085b449fcffe8b3816',
+    http:{
+        port:8000,
+        protocol:'http'
+    }
+}
+module.exports=config;
--- oauth.js
+++ oauth.js
@@ -2,21 +2,17 @@
  * Created by Administrator on 2016/7/6.
  */
 var OAuth=require("wechat-oauth");
-var https=require("https");
 var express=require('express');
 var urlPares=require('url');
+var MongoClient=reuqire('mongodb').MongoClient;
 var app=express();
-var appID="wx9a66afce31e4ec8b";
-var appSecret="c2b679dbd1ea6c0ffc99155123a25697";
+var url='mongodb://localhost:192.168.100.2/bookshelf';
+//var appID="wx9a66afce31e4ec8b";
+//var appSecret="c2b679dbd1ea6c0ffc99155123a25697";
 var scope='snsapi_userinfo';
-var redirectUrl="dev-facebook.beautifulreading.com/code";
-var Client=new OAuth(appID,appSecret);
-var url=Client.getAuthorizeURL(redirectUrl,'',scope);
-app.get('/',function (req,res) {
-    res.writeHead(302,{
-        'Location':url
-    })
-})
+//var redirectUrl="dev-facebook.beautifulreading.com/code";
+//var Client=new OAuth(appID,appSecret);
+//var url=Client.getAuthorizeURL(redirectUrl,'',scope);
 app.get('/code',function (req,res) {
     var rlt=urlPares.parse(req,true).query;
     var code=rlt.code;
@@ -25,9 +21,26 @@ app.get('/code',function (req,res) {
         var openID=result.data.openid;
         Client.getUser(openID,function (err,result) {
             var userInfo=result;
+            res.redirect("http://www.baidu.com");
+            MoogoCient.connect(url,function (err,db) {
+                console.log("connect to server")
+                db.collection('wechatusers').find({'openid':userInfo.openid}).toArray(function (err,docs) {
+                    if(!doc.length){
+                        insertDocument(db,userInfo,function () {
+
+                        })
+                    }
+                })
+            })
         })
     })
 })
+var insertDocument=function (db,doc,cb) {
+    db.collection('wechatusers').insertOne(doc,function (err,result) {
+        console.log("insert a document");
+        cb();
+    })
+};
 var server=app.listen(10000,function () {
     console.log("sucess!");
 })
